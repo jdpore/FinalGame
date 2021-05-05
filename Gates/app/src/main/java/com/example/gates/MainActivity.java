12eb71acd1b146e74232dad5bfe2fffc30636727
jdpore <pore.johndexter@ue.edu.ph>
2021-05-05 20:19:45 +0800

    Starting Layout

---
+++ Gates/app/src/main/java/com/example/gates/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.gates;
+
+import android.content.Intent;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,GameAndDifficulty.class));
+                finish();
+            }
+        }, 4000);
+    }
+}
