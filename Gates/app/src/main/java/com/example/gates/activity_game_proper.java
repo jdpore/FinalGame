b340c936cff48fb5d388c095d04e911d5920bd07
melcacho <cacho.michaelernestlorenz@ue.edu.ph>
2021-05-14 15:44:49 +0800

    Created Pop-Up & Buttons Programmatically

--- Gates/app/src/main/java/com/example/gates/activity_game_proper.java
+++ Gates/app/src/main/java/com/example/gates/activity_game_proper.java
@@ -1,14 +1,19 @@
 package com.example.gates;
 
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class activity_game_proper extends AppCompatActivity {
-    TextView title;
+    TextView title, round;
     LinearLayout columnOne;
+    ConstraintLayout popUp;
+    Button[] btnGrp = new Button[7];
     int game, difficulty;
     String[] games = new String[] {"Lesson", "Game One", "Game Two", "Game Three"};
     String[] difficulties = new String[] {"novice", "intermediate", "advance"};
@@ -19,6 +24,7 @@ public class activity_game_proper extends AppCompatActivity {
         setContentView(R.layout.activity_game_proper);
 
         title = findViewById(R.id.title);
+        popUp = findViewById(R.id.pop_up);
 
         Bundle extras = getIntent().getExtras();
         if(extras != null) {
@@ -27,26 +33,31 @@ public class activity_game_proper extends AppCompatActivity {
             title.setText(str);
         }
 
-        //gridInit();
+        gridInit();
+
+        popUp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                popUp.setVisibility(View.GONE);
+            }
+        });
     }
 
     public void gridInit() {
-        /*LinearLayout layout = new LinearLayout(this);
-        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) layout.getLayoutParams();
-
-        params.weight = 1;
-        layout.setLayoutParams(params);*/
         columnOne = findViewById(R.id.column_one);
-        Button btn = new Button(this);
-
-        LinearLayout layout = new LinearLayout(this);
-        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) layout.getLayoutParams();
-        params.weight = 1;
-        params.width = LinearLayout.LayoutParams.MATCH_PARENT;
-        btn.setHeight(0);
-        btn.setText("B");
-        btn.setLayoutParams(params);
-
-        columnOne.addView(btn);
+        round = findViewById(R.id.round);
+        for(int i = 0; i < 7; i++) {
+            Button btn = new Button(this);
+            btn.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT,
+                            1.0f));
+            btn.setHeight(0);
+            btn.setText(String.valueOf(i));
+            final int j = i;
+            btn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    round.setText(String.valueOf(j));
+                }
+            });
+            columnOne.addView(btn);
+        }
     }
 }
