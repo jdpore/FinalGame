1e1dc087f45fd869ce936e1b0806426a7ed21a1b
jdpore <pore.johndexter@ue.edu.ph>
2021-05-20 20:58:45 +0800

    Added pictures for circuits

---
+++ Gates/app/src/main/java/com/example/gates/activity_game_proper.java
@@ -0,0 +1,107 @@
+package com.example.gates;
+
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import androidx.annotation.Dimension;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class activity_game_proper extends AppCompatActivity {
+    TextView title, round;
+    LinearLayout columnOne, columnTwo, columnThree, columnFour, columnFive, column;
+    ConstraintLayout popUp;
+    Button[] btnGrp = new Button[7];
+    int game, difficulty;
+    int[] coord = new int[2];
+    String[] games = new String[] {"Lesson", "Game One", "Game Two", "Game Three"};
+    String[] difficulties = new String[] {"novice", "intermediate", "advance"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_proper);
+
+        title = findViewById(R.id.title);
+        round = findViewById(R.id.round);
+        //popUp = findViewById(R.id.pop_up);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int width = displayMetrics.widthPixels;
+
+        title.setTextSize(TypedValue.COMPLEX_UNIT_PX, (width * .14f));
+        round.setTextSize(TypedValue.COMPLEX_UNIT_PX, (width * .08f));
+
+        Bundle extras = getIntent().getExtras();
+        if(extras != null) {
+            game = extras.getInt("game");
+            String str = games[game];
+            title.setText(str);
+        }
+
+        gridInit();
+
+        /*popUp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                popUp.setVisibility(View.GONE);
+            }
+        });*/
+    }
+
+    public void gridInit() {
+        columnOne = findViewById(R.id.column_one);
+        for(int i = 0; i < 7; i++) {
+            Button btn = new Button(this);
+            btn.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT,
+                            1.0f));
+            btn.setText(String.valueOf(i));
+            final int j = i;
+            btn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    round.setText(String.valueOf(j));
+                }
+            });
+            columnOne.addView(btn);
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    // Shows the system bars by removing all the flags
+// except for the ones that make the content appear under the system bars.
+    private void showSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+}
